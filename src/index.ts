b716e0f21ebbffaad40f5885b3bec07e8f5668cf
quangtheHome <quangthe@DESKTOP-PD1HPBI>
2024-06-15 16:00:53 +0700

    chore: Update CORS options in index.ts

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,13 @@ import { serviceGoogleApi } from './services/serviceGoogleApi';
 const corsOptions = {
   allowedHeaders: ['authorization', 'Content-Type'],
   exposedHeaders: ['authorization'],
-  origin: 'http://localhost', // or 'http://localhost:<port>' if you're running the app on a specific port
+  origin: function (origin, callback) {
+    if (origin.startsWith('http://localhost')) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   preflightContinue: false,
 };
