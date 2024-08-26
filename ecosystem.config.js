7b79c7024542817930fa51bf132df191db72cfa7
BoyCoDon <thenq@dtp-education.com>
2024-08-26 16:02:45 +0700

    chore: Update environment variables for development and production

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,13 +4,15 @@ module.exports = {
             name: 'smart-shop-api',
             script: 'build/index.js', // Tệp chính để chạy ứng dụng
             instances: 'max', // Số lượng instances (max sẽ sử dụng tất cả các CPU có sẵn)
-            exec_mode: 'cluster', // Chế độ chạy (cluster hoặc fork)
+            exec_mode: 'fork', // Chế độ chạy (cluster hoặc fork)
             watch: true, // Theo dõi các thay đổi trong tệp để tự động khởi động lại
             env: {
-                NODE_ENV: 'development', // Biến môi trường cho chế độ phát triển
+                NODE_ENV: 'development',
+                PORT: 3002,
             },
             env_production: {
-                NODE_ENV: 'production', // Biến môi trường cho chế độ sản xuất
+                NODE_ENV: 'production',
+                PORT: 3002,
             },
             log_date_format: 'YYYY-MM-DD HH:mm Z', // Định dạng ngày giờ cho nhật ký
             error_file: './logs/err.log', // Tệp lưu lỗi
