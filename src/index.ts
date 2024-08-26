284ba116caaec4a433d382e2c64981642510be86
BoyCoDon <thenq@dtp-education.com>
2024-08-26 16:21:58 +0700

    chore: Update environment variables for development and production

--- src/index.ts
+++ src/index.ts
@@ -57,6 +57,7 @@ app.use(errorHandlerMiddleware);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+  console.log(`---------------------------------`);
 });
 
 export default app;
